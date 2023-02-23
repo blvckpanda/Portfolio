005c3dd9855020d12c95d14a1dcb0921893e6b39
blvckpanda <92569967+blvckpanda@users.noreply.github.com>
2023-02-23 17:17:47 +0300

    Portfolio

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
+import javascriptLogo from "./javascript.svg";
+import { setupCounter } from "./counter.js";
 
-document.querySelector('#app').innerHTML = `
+document.querySelector("#app").innerHTML = `
   <div>
   <section id="nav">
   <ul class="nav justify-content-end">
@@ -19,10 +19,15 @@ document.querySelector('#app').innerHTML = `
     <a class="nav-link" href="#" style="color:white">CONTACT</a>
   </li>
   
-</ul>
-</section>
-  
+  </ul>
+  </section>
+  <div id="home">
+  <p class="greeting">Hello, I'm <br>
+  a Front End Developer</p>
   </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+  </div>
+  
+`;
+
+setupCounter(document.querySelector("#counter"));
