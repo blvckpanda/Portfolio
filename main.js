3dfd97ba5f9595701135f2802c97208f54fa582d
blvckpanda <92569967+blvckpanda@users.noreply.github.com>
2023-02-23 17:40:24 +0300

    Portfolio

--- main.js
+++ main.js
@@ -5,22 +5,22 @@ import { setupCounter } from "./counter.js";
 document.querySelector("#app").innerHTML = `
   <div>
   <section id="nav">
-  <ul class="nav justify-content-end">
-  <li class="nav-item">
-    <a class="nav-link" href="#" style="color:white">HOME</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#" style="color:white">SKILL-SET</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#" style="color:white">PROJECTS</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#" style="color:white">CONTACT</a>
-  </li>
-  
-  </ul>
+    <ul class="nav justify-content-end">
+    <li class="nav-item">
+      <a class="nav-link" href="#" style="color:#fff;href:#;">HOME</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#" style="color:#fff;href:#;">SKILL-SET</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#" style="color:#fff;href:#;">PROJECTS</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#" style="color:#fff;href:#;">CONTACT</a>
+    </li>
+    </ul>
   </section>
+  
   <div id="home">
   <p class="greeting">Hello, I'm <br>
   a Front End Developer</p>
