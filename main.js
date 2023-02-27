2315fcf9020dd7c650d4a1d9b73f11559d5dfc1c
blvckpanda <92569967+blvckpanda@users.noreply.github.com>
2023-02-27 22:37:27 +0300

    Portfolio

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-const body = document.querySelector("body");
-    const toggle = document.querySelector("#toggle");
-    const sunIcon = document.querySelector(".toggle .bxs-sun");
-    const moonIcon = document.querySelector(".toggle .bx-moon");
-    
-    toggle.addEventListener("change", () => {
-      body.classList.toggle("dark");
-      sunIcon.className =
-        sunIcon.className == "bx bxs-sun" ? "bx bx-sun" : "bx bxs-sun";
-      moonIcon.className =
-        moonIcon.className == "bx bxs-moon" ? "bx bx-moon" : "bx bxs-moon";
-    });
-
 document.querySelector("#app").innerHTML = ` `;
 document.querySelector("#hero").innerHTML = ``;
 document.querySelector("#about").innerHTML = ``;
