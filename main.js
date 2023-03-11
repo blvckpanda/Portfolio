ea1c1e4905e81ede385c9c610fe81baeeb1f7ea3
blvckpanda <92569967+blvckpanda@users.noreply.github.com>
2023-03-11 21:52:32 +0300

    Portfolio

--- main.js
+++ main.js
@@ -1,5 +1,1 @@
 document.querySelector("#app").innerHTML = ` `;
-document.querySelector("#hero").innerHTML = ``;
-document.querySelector("#about").innerHTML = ``;
-document.querySelector("#blog").innerHTML = ``;
-document.querySelector("#contact").innerHTML = ``;
